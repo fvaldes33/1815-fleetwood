bdf789114fa25d2fa66ae43c232a6aadf45877e2
Franco Valdes <franco.valdes89@gmail.com>
2020-10-12 00:55:13 -0400

    og meta data

--- components/HeadData.tsx
+++ components/HeadData.tsx
@@ -13,6 +13,19 @@ export default function HeadData() {
       <meta name="robots" content="follow, index" />
       <link rel="canonical" href="https://1815fleetwood.com" />
       <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;700&display=swap" rel="stylesheet" />
+
+      <meta property="og:type" content="website"></meta>
+      <meta property="og:site_name" content="1815 Fleetwood Dr"></meta>
+      <meta property="og:url" content="https://1815fleetwood.com"></meta>
+      <meta property="og:title" content="1815 Fleetwood Drive"></meta>
+      <meta property="og:description" content="Welcome to your next home in HIGHLY sought after Bryant Park! This single family home is situated on a premium lot with a 5 ft extension upgrade off of the living room! The main level features an open floorplan with hardwoods; a spacious kitchen with stainless appliances, granite countertops, tile backsplash, a large pantry and a center island. Upstairs, you’ll find a" />
+      <meta property="og:image" content="/assets/house-front.jpg"></meta>
+      <meta property="og:image:type" content="image/jpeg"></meta>
+      <meta name="twitter:card" content="summary_large_image"></meta>
+      <meta name="twitter:url" content="https://1815fleetwood.com"></meta>
+      <meta name="twitter:title" content="1815 Fleetwood Drive"></meta>
+      <meta name="twitter:description" content="Welcome to your next home in HIGHLY sought after Bryant Park! This single family home is situated on a premium lot with a 5 ft extension upgrade off of the living room! The main level features an open floorplan"></meta>
+      <meta name="twitter:image" content="/assets/house-front.jpg"></meta>
     </Head>
   );
 }
