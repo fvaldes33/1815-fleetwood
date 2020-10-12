3ee9751679191972d8f018824612577b70b87418
Franco Valdes <franco.valdes89@gmail.com>
2020-10-12 01:05:09 -0400

    google analytics

--- components/HeadData.tsx
+++ components/HeadData.tsx
@@ -6,7 +6,7 @@ export default function HeadData() {
       <title>1815 Fleetwood Dr | Charlotte NC 28208 | Bryant Park</title>
       <link rel="icon" href="/favicon.ico" />
       <meta charSet="utf-8" />
-      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="keywords" content="1815 Fleetwood Drive, Fleetwood Drive, Charlotte Real Estate, Nick Cabaniss, CRI Group, Bryant Park" />
       <meta name="description" content="Welcome to your next home in HIGHLY sought after Bryant Park! This single family home is situated on a premium lot with a 5 ft extension upgrade off of the living room! The main level features an open floorplan with hardwoods; a spacious kitchen with stainless appliances, granite countertops, tile backsplash, a large pantry and a center island. Upstairs, you’ll find a" />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,25 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=UA-180297725-1`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-180297725-1', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
+        </Head>
         <body className="overflow-x-hidden">
           <Main />
           <NextScript />
@@ -19,4 +37,4 @@ class MyDocument extends Document {
   }
 }
 
-export default MyDocument
+export default MyDocument;
